1ec866856266116180171e7fe2a4efef93bbc058
adula <whitenora807@gmail.com>
2025-05-21 17:31:58 +0900

    second commit

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-# git 테스트를 위한 코드입니다
+# git 테스트를 위한 코드입니다. 아래는 깃 코드입니다.
+# git init (initialize, 설정)
+# git add . (전부 추가한다)
+# git status (상태를 알려 줌. 필수 아님.)
+# git commit -m "first commit" (히스토리 이름) 
+# git remote add origin https://github.com/Adula15445/test-project.git (레포지토리 링크, github 사이트에서 확인)
+# git branch -M main (branch 이름을 main으로 강제로 바꾼다)
+# git push -u origin main (업로드)
+
 print ("Hello Git! Hello Adula!")
