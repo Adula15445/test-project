9e606a537be6577ab896314472832de38500f972
adula <whitenora807@gmail.com>
2025-05-21 17:38:47 +0900

    third commit

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # git 테스트를 위한 코드입니다. 아래는 깃 코드입니다.
+# 깃 초기설정
 # git init (initialize, 설정)
 # git add . (전부 추가한다)
 # git status (상태를 알려 줌. 필수 아님.)
@@ -7,4 +8,10 @@
 # git branch -M main (branch 이름을 main으로 강제로 바꾼다)
 # git push -u origin main (업로드)
 
+# 코드를 수정하고 재업로드
+# git add .
+# git commit -m "second commit" (second, third..)
+# git puch origin main
+
 print ("Hello Git! Hello Adula!")
+print ("이 코드는 수정되었습니다")
